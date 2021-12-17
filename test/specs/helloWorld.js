69c92d6d520a3940f3ce818711e3af86fcc261bd
josdem <joseluis.delacruz@gmail.com>
2021-12-17 13:57:01 -0600

    #1 Adding geolocation spec

--- test/specs/helloWorld.js
+++ test/specs/helloWorld.js
@@ -1,11 +1,12 @@
-const assert = require('assert')
+const assert = require("assert")
 
-describe('Loading webpage', () => {
-
-   it('validates website title', async() => {
-       await browser.url('https://josdem.io/')
-       const title = await browser.getTitle()
-       assert.strictEqual(title, "josdem")
-   })
-
+describe("Checking locations", () => {
+  it("validate I am in Michigan", async () => {
+    await browser.url("https://www.where-am-i.net/")
+    await browser.cdp("Emulation", "setGeolocationOverride", {
+      latitude: 42.3173603,
+      longitude: -83.6826172,
+      accuracy: 1,
+    })
+  })
 })
