cf37fcb09e5f596a2fa8eb2b76e8bff4bc681ead
josdem <joseluis.delacruz@gmail.com>
2021-12-17 14:49:49 -0600

    #1 Adding location page object

---
+++ test/pageobjects/location.page.js
@@ -0,0 +1,13 @@
+class LocationPage {
+    get locationButton() {
+        return $('[id=btnMyLocation]')
+    }
+
+    async clickOnLocationButton() {
+        const button = await this.locationButton
+        await expect(button).toBeExisting()
+        await button.click()
+    }
+}
+
+module.exports = new LocationPage()
--- test/specs/geolocation.spec.js
+++ test/specs/geolocation.spec.js
@@ -1,6 +1,9 @@
+const LocationPage = require("../pageobjects/location.page")
+
 describe("Checking locations", () => {
   it("validate I am in Michigan", async () => {
     await browser.url("https://www.where-am-i.net/")
+    await LocationPage.clickOnLocationButton()
     await browser.pause(3000)
     await browser.cdp("Emulation", "setGeolocationOverride", {
       latitude: 42.3173603,
@@ -8,6 +11,7 @@ describe("Checking locations", () => {
       accuracy: 1,
     })
     await browser.pause(3000)
+    await LocationPage.clickOnLocationButton()
     await browser.cdp("Emulation", "setGeolocationOverride", {
       latitude: 20.6743943,
       longitude: -103.3874128,
