02823ba5206075879f79d7c21a19acc3747f4449
josdem <joseluis.delacruz@gmail.com>
2021-12-17 15:18:47 -0600

    #1 Adding goes to Amsterdam locaTION

--- test/specs/geolocation.spec.js
+++ test/specs/geolocation.spec.js
@@ -1,7 +1,8 @@
 const LocationPage = require("../pageobjects/location.page")
+const waitingTime = 3000
 
 describe("Checking locations", () => {
-  it("validate I am in Michigan", async () => {
+  it("goes from Ann Arbor to Guadalajara to Amsterdam", async () => {
     await browser.url("https://www.where-am-i.net/")
     await browser.cdp("Emulation", "setGeolocationOverride", {
       latitude: 42.3173603,
@@ -9,13 +10,20 @@ describe("Checking locations", () => {
       accuracy: 1,
     })
     await LocationPage.clickOnLocationButton()
-    await browser.pause(3000)
+    await browser.pause(waitingTime)
     await browser.cdp("Emulation", "setGeolocationOverride", {
       latitude: 20.6743943,
       longitude: -103.3874128,
       accuracy: 1,
     })
     await LocationPage.clickOnLocationButton()
-    await browser.pause(3000)
+    await browser.pause(waitingTime)
+    await browser.cdp("Emulation", "setGeolocationOverride", {
+      latitude: 52.369357,
+      longitude: 4.9012204,
+      accuracy: 1,
+    })
+    await LocationPage.clickOnLocationButton()
+    await browser.pause(waitingTime)
   })
 })
