a953b4a05166686fbed0db22380349c8fdcfac3a
josdem <joseluis.delacruz@gmail.com>
2021-12-17 14:34:42 -0600

    #1 Deleting geolocation utils

--- test/specs/geolocation.spec.js
+++ test/specs/geolocation.spec.js
@@ -1,14 +1,18 @@
-const assert = require("assert")
-const utils = require("../utils/geolocation.util")
-
 describe("Checking locations", () => {
   it("validate I am in Michigan", async () => {
     await browser.url("https://www.where-am-i.net/")
-    await utils.geolocation()
+    await browser.pause(3000)
     await browser.cdp("Emulation", "setGeolocationOverride", {
       latitude: 42.3173603,
       longitude: -83.6826172,
       accuracy: 1,
     })
+    await browser.pause(3000)
+    await browser.cdp("Emulation", "setGeolocationOverride", {
+      latitude: 20.6743943,
+      longitude: -103.3874128,
+      accuracy: 1,
+    })
+    await browser.pause(3000)
   })
 })
--- test/utils/geolocation.util.js
+++
@@ -1,5 +0,0 @@
-const geolocation = () => {
-  console.log("Hello world!")
-}
-
-module.exports.geolocation = geolocation
