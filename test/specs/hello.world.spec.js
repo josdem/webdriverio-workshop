34ac1a1e138ec73cdbd65400a72a09d546c33717
josdem <joseluis.delacruz@gmail.com>
2023-02-23 14:10:05 -0500

    #5 Adding simple validation

---
+++ test/specs/hello.world.spec.js
@@ -0,0 +1,8 @@
+const assert = require("assert")
+
+describe("Create a simple validation", () => {
+  it("Validates a true statement", async () => {
+    console.log("Hello World!")
+    assert.ok(1 === 1)
+  })
+})
