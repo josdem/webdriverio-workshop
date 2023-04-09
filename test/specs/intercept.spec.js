fb5de1fbe54a90ba03c95de63b8fc13842790b50
josdem <joseluis.delacruz@gmail.com>
2023-04-09 15:46:14 -0400

    #6 Setting service URL

---
+++ test/specs/intercept.spec.js
@@ -0,0 +1,11 @@
+const assert = require("assert")
+const properties = require(`../properties/test.properties`)
+
+describe("consuming an RESTful API", () => {
+  it("Getting categories from service", async () => {
+    const url = `${properties.serviceUrl}/categories/`
+    browser.url(url)
+    browser.setupInterceptor()
+    browser.expectRequest("GET", "/", 200)
+  })
+})
