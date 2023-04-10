b7c871e1905a5ff8d3b8ebf88d3dc0d5f20cd475
josdem <joseluis.delacruz@gmail.com>
2023-04-10 11:35:34 -0400

    #6 Adding home page select customer address

--- test/specs/intercept.spec.js
+++ test/specs/intercept.spec.js
@@ -3,9 +3,15 @@ const properties = require(`../properties/test.properties`)
 const HomePage = require("../pageobjects/home.page")
 
 describe("intercepting search request", () => {
-  it("sends zip code, city and state", async () => {
+  it("opens home page", async () => {
     await HomePage.open()
-    browser.setupInterceptor()
-    browser.expectRequest("GET", "/", 200)
+    await browser.url(properties.dominosUrl);
+    await browser.setupInterceptor()
+  })
+
+  it("sends zip code, city and state", async () => {
+    await HomePage.selectZipCodeCityAndState()
+    await browser.pause(2000)
+    await browser.expectRequest("GET", "/search", 400)
   })
 })
