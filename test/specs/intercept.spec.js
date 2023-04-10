fe5dd26cc797c10c56b59fe76342094c32cddd02
josdem <joseluis.delacruz@gmail.com>
2023-04-10 12:16:33 -0400

    #6 Validates service method

--- test/specs/intercept.spec.js
+++ test/specs/intercept.spec.js
@@ -1,3 +1,4 @@
+const assert = require("assert")
 const properties = require(`../properties/test.properties`)
 
 const HomePage = require("../pageobjects/home.page")
@@ -14,9 +15,14 @@ describe("intercepting search request", () => {
   })
 
   it("validate search request", async () => {
-    await browser.expectRequest("POST", "/search", 200)
+    await browser.expectRequest("POST", "https://dominos.josdem.io/search", 200)
     await HomePage.clickOnSearchLocation()
     await browser.pause(2000)
     await browser.assertRequests()
+  })
+
+  it("validates service method title", async () => {
+    const title = await browser.getTitle()
+    assert.strictEqual(title, "Service Method")
   })
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -78,7 +78,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: "http://localhost",
+  baseUrl: "https://dominos.josdem.io",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
