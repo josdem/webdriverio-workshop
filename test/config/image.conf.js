b57dd57c6157b8f9953cb33a56781ac298134017
josdem <joseluis.delacruz@gmail.com>
2023-05-09 16:30:58 -0400

    #7 Cleaning configuration

--- test/config/image.conf.js
+++ test/config/image.conf.js
@@ -4,32 +4,14 @@ const { join } = require('path')
 config.services = config.services.concat(["chromedriver"])
 
 config.services = config.services.concat([
-  ['image-comparison',
-  // The options
-  {
-      // Some options, see the docs for more
-      baselineFolder: join(process.cwd(), './tests/sauceLabsBaseline/'),
-      formatImageName: '{tag}-{logName}-{width}x{height}',
-      screenshotPath: join(process.cwd(), '.tmp/'),
+  ['image-comparison',{
+      baselineFolder: join(process.cwd(), './baselineImages/'),
+      formatImageName: '{tag}-{logName}',
+      screenshotPath: join(process.cwd(), './actualImages'),
       savePerInstance: true,
       autoSaveBaseline: true,
       blockOutStatusBar: true,
-      blockOutToolBar: true,
-      // NOTE: When you are testing a hybrid app please use this setting
-      isHybridApp: true,
-      // Options for the tabbing image
-      tabbableOptions:{
-          circle:{
-              size: 18,
-              fontSize: 18,
-              // ...
-          },
-          line:{
-              color: '#ff221a', // hex-code or for example words like `red|black|green`
-              width: 3,
-          },
-      }
-      // ... more options
+      blockOutToolBar: true
   }],
 ],)
 
