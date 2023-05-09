9f1b8747f06f7affe88822dbac272cf599799f5f
josdem <joseluis.delacruz@gmail.com>
2023-05-09 16:50:30 -0400

    #7 Formatting code

--- test/config/image.conf.js
+++ test/config/image.conf.js
@@ -1,19 +1,22 @@
 const { config } = require("../../wdio.conf")
-const { join } = require('path')
+const { join } = require("path")
 
 config.services = config.services.concat(["chromedriver"])
 
 config.services = config.services.concat([
-  ['image-comparison',{
-      baselineFolder: join(process.cwd(), './baselineImages'),
-      formatImageName: '{tag}-{logName}',
-      screenshotPath: join(process.cwd(), './actualImages'),
+  [
+    "image-comparison",
+    {
+      baselineFolder: join(process.cwd(), "./baselineImages"),
+      formatImageName: "{tag}-{logName}",
+      screenshotPath: join(process.cwd(), "./actualImages"),
       savePerInstance: true,
       autoSaveBaseline: true,
       blockOutStatusBar: true,
-      blockOutToolBar: true
-  }],
-],)
+      blockOutToolBar: true,
+    },
+  ],
+])
 
 config.capabilities = [
   {
--- test/specs/image.comparison.spec.js
+++ test/specs/image.comparison.spec.js
@@ -1,13 +1,13 @@
-describe('Example', () => {
-    beforeEach(async () => {
-      await browser.url('https://webdriver.io')
-    })
-  
-    it("should save some screenshots", async () => {
-      await browser.saveScreen('homePage', {})
-    })
-  
-    it("should compare successful with a baseline", async () => {
-      expect(await browser.checkScreen('homePage', {})).toEqual(0)
-    })
+describe("Example", () => {
+  beforeEach(async () => {
+    await browser.url("https://webdriver.io")
   })
+
+  it("should save some screenshots", async () => {
+    await browser.saveScreen("homePage", {})
+  })
+
+  it("should compare successful with a baseline", async () => {
+    expect(await browser.checkScreen("homePage", {})).toEqual(0)
+  })
+})
